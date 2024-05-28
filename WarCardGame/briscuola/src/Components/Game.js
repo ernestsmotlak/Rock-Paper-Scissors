91a5dda8bc51262f8816258504477fac47d8aaad
ernestsmotlak <ernestsmotlak@gmail.com>
2024-05-28 19:25:13 +0200

    The game now kinda works. It needs changes so that the buttons are disabled after pressing and awaiting the other players choice.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- WarCardGame/briscuola/src/Components/Game.js
+++ WarCardGame/briscuola/src/Components/Game.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 const Game = (props) => {
     const [gameHistory, setGameHistory] = useState([]);
-    const results = ['tie, play again!', 'won', 'lost'];
+    const results = ['tie!', 'won', 'lost'];
+    const [wins, setWins] = useState(0);
 
     useEffect(() => {
         const playGame = (me, opponent) => {
@@ -21,21 +22,64 @@ const Game = (props) => {
             setGameHistory(prevHistory => [...prevHistory, result]);
         };
 
-        playGame(props.choice, props.opponentChoice);
+        if (props.choice && props.opponentChoice) {
+            playGame(props.choice, props.opponentChoice);
+        }
     }, [props.choice, props.opponentChoice]);
+
+    useEffect(() => {
+        const numberOfWins = (arr) => {
+            let count = 0;
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i] === 'won') {
+                    count += 1;
+                }
+            }
+            return count;
+        };
+        setWins(numberOfWins(gameHistory));
+    }, [gameHistory]);
 
     return (
         <div>
             <h4>Your Choice: {props.choice}</h4>
             <h4>Opponent's Choice: {props.opponentChoice}</h4>
             <br />
-            Current game: 
-            {gameHistory[gameHistory.length - 1]}
-            <br />
-            Results: {gameHistory}
-            <br />
+            {gameHistory.length > 0 && (
+                <>
+                    <div className='w-25 mx-auto'>
+                        <div>Current game: {gameHistory[gameHistory.length - 1]}</div>
+                        <div>Results: {gameHistory.join(', ')}</div>
+                    </div>
+
+                    <br />
+
+                    <div className="container text-center w-25">
+                        <div className="row">
+                            <div className="col">
+                                Me:
+                                <br />
+                                <div className='fs-2 text-primary bg-secondary-subtle'>
+                                    {wins}
+                                </div>
+                            </div>
+                            <div className="col">
+                                <br />
+                                <p className='fs-2'>:</p>
+                            </div>
+                            <div className="col w-25">
+                                Opponent:
+                                <br />
+                                <div className='fs-2 text-danger bg-secondary-subtle'>
+                                    {gameHistory.length - wins}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
-    )
+    );
 }
 
 export default Game;
